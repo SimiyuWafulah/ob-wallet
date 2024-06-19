bf63d972d947603b9d968b76cb0b5c190f32ec90
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-06-19 12:00:57 +0300

    implement right sidebar and bank card layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import RightSidebar from '@/components/RightSidebar'
 
 const Page = () => {
+    const loggedIn ={firstName: 'Kelvin', lastName: 'Simiyu', email:'simiyukelvin@gmail.com'};
     return (
-        <div>Page</div>
+        <section className="home">
+            <div className ="home-content">
+                <header className= "home-header">
+                    <HeaderBox
+                    type = "greeting"
+                    title="Welcome"
+                    user ={loggedIn?.firstName || 'Guest'}
+                    subtext ="Access and manage your account and transactions efficiently"
+                    />
+                    <TotalBalanceBox
+                    accounts ={[]}
+                    totalBanks ={1}
+                    totalCurrentBalance={1250.35}
+                    />
+                </header>
+                RECENT TRANSACTIONS
+            </div>
+            <RightSidebar
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance : 1230.50}, {currentBalance :500}]}
+            />
+        </section>
     )
 }
 
-export default Page
+export default Page 
